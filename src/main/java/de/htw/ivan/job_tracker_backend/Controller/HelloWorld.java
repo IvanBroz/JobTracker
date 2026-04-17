62b1e76df94a205350e3ee050be46023cd2c4c81
IvanBroz <ivan.brozincevic@student.htw-berlin.de>
2026-04-17 09:03:18 +0200

    init

---
+++ src/main/java/de/htw/ivan/job_tracker_backend/Controller/HelloWorld.java
@@ -0,0 +1,14 @@
+package de.htw.ivan.job_tracker_backend.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorld {
+
+    @GetMapping(path = "/")
+    public ModelAndView showHelloWorldPage(){
+        return new ModelAndView("helloworld");
+    }
+}
---
+++ src/test/java/de/htw/ivan/job_tracker_backend/JobTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htw.ivan.job_tracker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
