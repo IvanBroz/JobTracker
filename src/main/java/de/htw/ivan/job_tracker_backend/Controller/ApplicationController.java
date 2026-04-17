eaa6c9cb18e6c0a5352b1fdc5764e07d005f9937
IvanBroz <ivan.brozincevic@student.htw-berlin.de>
2026-04-17 10:45:01 +0200

    M1 mit wiedergabe der Liste ohne HTML

--- src/main/java/de/htw/ivan/job_tracker_backend/Controller/ApplicationController.java
+++ src/main/java/de/htw/ivan/job_tracker_backend/Controller/ApplicationController.java
@@ -1,14 +1,24 @@
 package de.htw.ivan.job_tracker_backend.Controller;
 
+import de.htw.ivan.job_tracker_backend.Entity.Application;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class ApplicationController {
 
     @GetMapping(path = "/")
-    public ModelAndView showApplicationsPage() {
-        return new ModelAndView("applications");
+    public ResponseEntity<List<Application>> getApplications() {
+        List<Application> applications = List.of(
+                new Application(1L, "Siemens", "Junior Developer", "Bewerbung gesendet"),
+                new Application(2L, "BMW", "Backend Developer", "Interview"),
+                new Application(3L, "SAP", "Software Engineer", "Absage")
+        );
+
+        return ResponseEntity.ok(applications);
     }
 }
