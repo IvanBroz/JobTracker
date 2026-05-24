b5474e78a76eab09403d3c4fbd797df5652e6e09
IvanBroz <ivan.brozincevic@student.htw-berlin.de>
2026-05-24 15:53:35 +0200

    Add applications endpoint for frontend

--- src/main/java/de/htw/ivan/job_tracker_backend/Controller/ApplicationController.java
+++ src/main/java/de/htw/ivan/job_tracker_backend/Controller/ApplicationController.java
@@ -2,17 +2,19 @@ package de.htw.ivan.job_tracker_backend.Controller;
 
 import de.htw.ivan.job_tracker_backend.Entity.Application;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
+@CrossOrigin(origins = "*")
 public class ApplicationController {
 
-    @GetMapping(path = "/")
+    @GetMapping(path = "/applications")
     public ResponseEntity<List<Application>> getApplications() {
+
         List<Application> applications = List.of(
                 new Application(1L, "Siemens", "Junior Developer", "Bewerbung gesendet"),
                 new Application(2L, "BMW", "Backend Developer", "Interview"),
